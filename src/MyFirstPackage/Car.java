309d3895c767b4b5ad3352fdbd15e2afacd5415a
Musfata Erol saglam <mustafaerol_34@hotmail.com>
2019-06-13 16:17:22 -0500

    second commit

--- src/MyFirstPackage/Car.java
+++ src/MyFirstPackage/Car.java
@@ -5,7 +5,8 @@ public class Car {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello guys What are you doing");
-
+		System.out.println("hello guys What are you doing");
+		System.out.println("hello guys What are you doing");
 	}
 
 }
